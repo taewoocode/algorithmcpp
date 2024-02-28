d9718c0c0bf88e5c85fa3905f7d45ccae976d405
박태우 <qkrxodn6035@gmail.com>
2024-02-28 13:44:10 +0900

    basic5

---
+++ basic4.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+double a = 1.23456789;
+int main(){
+	cout << a << endl;
+	cout.precision(7);
+	cout << a << endl;
+	return 0;
+}
+
